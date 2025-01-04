8db4f5c5c40645c374ca37490e7d88c912a7ed53
EvgenKh <hamitcevich@gmail.com>
2025-01-05 01:56:12 +0300

    feat: test 53 passing

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let result = '';
+  let chars = str.split('');
+  let count = 1;
+
+  for (let i = 0; i < chars.length; i += 1) {
+
+    if (chars[i] === chars[i + 1]) {
+      count += 1;
+
+    } else {
+      if (count === 1) {
+        result += chars[i];
+
+      } else {
+        result += `${count}${chars[i]}`;
+        count = 1;
+      }
+    }
+
+  }
+
+  return result;
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,33 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+
+  let {repeatTimes, separator = '+', addition = '', additionRepeatTimes, additionSeparator = '|'} = options;
+
+  String(addition);
+  String(str);
+
+  result = (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+  result = str + result;
+
+  result = (result + separator).repeat(repeatTimes - 1) + result;
+
+  return result;
 }
 
 module.exports = {
